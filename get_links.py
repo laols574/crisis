181939cc461a9adcd260490fd6181591ecec99bb
laols574 <laols574@email.arizona.edu>
2021-06-16 15:31:10 -0700

    a webscraper for breitbart links

---
+++ get_links.py
@@ -0,0 +1,35 @@
+"""
+get links from news websites: a simple web scraper
+author: lauren olson
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+f = open("breitbart_links.txt", "w")
+
+urls = ["https://www.breitbart.com/tag/joe-biden/", "https://www.breitbart.com/tag/critical-race-theory/", "https://www.breitbart.com/tag/coronavirus/", "https://www.breitbart.com/tag/immigration/", "https://www.breitbart.com/tag/masters-of-the-universe/"]
+
+tally = 0
+for url in urls:
+
+	page = requests.get(url)
+	i = 2
+	
+	while(page.status_code == 200):
+		soup = BeautifulSoup(page.content, 'html.parser')
+	
+		for o in soup.find_all("a"):
+			link = o.get('href')
+			
+			if(link != None and link[0] != "#"):
+				f.write(link + "\n")
+				tally += 1
+
+		page = requests.get(url + "page/" + str(i) + "/")
+		i += 1
+
+print("you collected " + str(tally) + " links")
+f.close()
